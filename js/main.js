d0fc7842da8c831f62c53d4943122d485f46b67e
Aahz101 <j9dawg@icloud.com>
2025-04-08 23:38:23 -0500

    get values from inputs in html

--- js/main.js
+++ js/main.js
@@ -3,17 +3,31 @@ const searchForm = document.getElementById("search-form");
 const searchBtn = document.getElementById("search-btn");
 const searchBox = document.getElementById("search-box");
 const resultsContainer = document.getElementById(".js-results-container");
-const limit = document.getElementById("img-count-id");
+let limit = document.getElementById("img-count-id");
+let searchTerm = document.getElementById("search-box");
 const apiurl = "http://api.giphy.com/v1/gifs/search";
 const apiKey = "rJs1dhQNXII3vND3b8amLn6LwHSb35IW";
 
+function setValues()
+{
+searchBox.textContent
+  searchTerm = document.getElementById("search-box").value;
+  limit = document.getElementById("img-count-id").value;
+}
 
 async function searchClicked(event) {
   event.preventDefault();
+  setValues();
  
   let searchDoc = await fetch(`https://api.giphy.com/v1/gifs/search?q=dog&api_key=${apiKey}&limit=${limit}`)
 console.log(searchDoc);
+let images = searchDoc.images;
+resultsContainer. renderImages(images);
 }
 
-      
+function renderImages(images) {
+
+}      
+
+
 searchBtn.addEventListener("click", searchClicked);
