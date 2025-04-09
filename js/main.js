74647f67557d50917a30476cc94af7ab38b1302e
Aahz101 <j9dawg@icloud.com>
2025-04-08 23:26:39 -0500

    proper fetch implemented

--- js/main.js
+++ js/main.js
@@ -4,50 +4,16 @@ const searchBtn = document.getElementById("search-btn");
 const searchBox = document.getElementById("search-box");
 const resultsContainer = document.getElementById(".js-results-container");
 const limit = document.getElementById("img-count-id");
-const apiurl = "api.giphy.com/v1/gifs/search";
+const apiurl = "http://api.giphy.com/v1/gifs/search";
 const apiKey = "rJs1dhQNXII3vND3b8amLn6LwHSb35IW";
 
-// ${"search-btn"}.click(()=>{
-//   let userInput = $("#search-box").val();
-//   $.ajax({
-//     url: apiurl,
-//     apiKey: apiKey,
-//     q: userInput,
-//     limit: limit.value,
-//   }).done((res)=> {
-//     console.log(res);
-//   });
-// })
 
-// searchBtn.clicked = function (event) {
-//   preventDefault();
-//   debugger;
-//   let searchDoc = $.ajax({
-//     url: apiurl,
-//     apiKey: apiKey,
-//   }).done(function () {
-//     $(this).addClass("done");
-
-//   console.log(searchDoc);
-//   });
-
-function searchClicked(event) {
+async function searchClicked(event) {
   event.preventDefault();
-  var xhr = $.get(`${apiurl}?q=${searchBox}&api_key=${apiKey}&limit=${limit}`);
-  xhr.done(function (data) {
-    console.log("success got data", data);
-  });
-
-  //   event.preventDefault();
-  //   debugger;
-  //     let searchDoc = `$.ajax({
-  //       url: apiurl,
-  //       apiKey: apiKey,
-  //     }).done(function () {
-  //       $(this).addClass("done");
-  //     });
-  //     `
-
-  // console.log(searchDoc);
+ 
+  let searchDoc = await fetch(`https://api.giphy.com/v1/gifs/search?q=dog&api_key=${apiKey}&limit=${limit}`)
+console.log(searchDoc);
 }
+
+      
 searchBtn.addEventListener("click", searchClicked);
